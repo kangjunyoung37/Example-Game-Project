59e39ea39b66d50382609398b53c4894ca260599
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-03-11 13:51:20 +0900

    모바일 게임

---
+++ mobile game/Assets/scrips/dongle.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dongle : MonoBehaviour
+{
+    public bool isDrag;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (isDrag)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float leftBorder = -4.2f + transform.localScale.x / 2f;
+            float rightBorder = 4.2f - transform.localScale.x / 2f;
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
+
+            mousePos.y = 8;
+            mousePos.z = 0;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+
+        }
+
+
+
+    }
+    public void Drag()
+    {
+        isDrag = true;
+
+    }
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+
+
+}
